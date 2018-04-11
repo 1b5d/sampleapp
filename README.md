58ff2e66274d3f7d0e7ebe62221bd007311b793e
abed-hawa <abdelrahman.haoua@gmail.com>
2018-04-11 15:36:08 +0200

    add tests and docker compose config files

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-#sample app
+# sample app
 
+### running app
 
-To run the app using the docker container, please use the following command:
+To run the app, please use the following command:
+
+`docker-compose up`
+
+Then you can check the app in the browser by visiting:
+
+`http://localhost:8000/`
+
+### running tests
+
+To run the tests, you can use the following command:
+
+ `docker-compose -f ./docker-compose.test.yml up`
  
--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -6,3 +6,61 @@ import pytest
 def test_home(client, django_db_setup):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK.value
+
+
+@pytest.mark.parametrize('name', [
+    'Demo Site',
+    'ABC Site',
+    'XYZ Site'
+])
+@pytest.mark.django_db
+def test_sites(name, client, django_db_setup):
+    response = client.get('/sites/')
+
+    assert response.status_code == HTTPStatus.OK.value
+    assert name in response.content.decode('utf8')
+
+
+@pytest.mark.parametrize('site_id,a_value,b_value', [
+    (1, '12.00', '16.00'),
+    (1, '20.00', '100.00'),
+    (1, '20.00', '80.00'),
+    (2, '5.00', '15.00'),
+    (3, '5.00', '15.00'),
+    (3, '5.00', '15.00'),
+])
+@pytest.mark.django_db
+def test_site(site_id, a_value, b_value, client, django_db_setup):
+    response = client.get('/sites/{site_id}/'.format(site_id=site_id))
+
+    assert response.status_code == HTTPStatus.OK.value
+    assert a_value in response.content.decode('utf8')
+    assert b_value in response.content.decode('utf8')
+
+
+@pytest.mark.parametrize('a_value,b_value', [
+    ('52.00', '196.00'),
+    ('5.00', '15.00'),
+    ('10.00', '30.00'),
+])
+@pytest.mark.django_db
+def test_sum(a_value, b_value, client, django_db_setup):
+    response = client.get('/summary/')
+
+    assert response.status_code == HTTPStatus.OK.value
+    assert a_value in response.content.decode('utf8')
+    assert b_value in response.content.decode('utf8')
+
+
+@pytest.mark.parametrize('a_value,b_value', [
+    ('17.33', '65.33'),
+    ('5.00', '15.00'),
+    ('5.00', '15.00'),
+])
+@pytest.mark.django_db
+def test_sum(a_value, b_value, client, django_db_setup):
+    response = client.get('/summary-average/')
+
+    assert response.status_code == HTTPStatus.OK.value
+    assert a_value in response.content.decode('utf8')
+    assert b_value in response.content.decode('utf8')
